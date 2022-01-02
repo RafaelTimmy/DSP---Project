106db053b1b83f6f5d195b54b841ce71ac636b76
Charles Reynold <Charlesreynold.gmail.com>
2022-01-02 19:49:52 +0800

    init

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import cv2
+import time
+import os
 
+wCam, hCam = 648, 488  
+
+vid = cv2.VideoCapture(0)  
+vid.set(3, wCam)
+vid.set(4, hCam) 
+
+while True:
+    success, img = vid.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
